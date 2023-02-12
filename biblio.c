949e5badc1c54c66b4d4c0869b04eaa354a63ef2
sarah-deseille <sarah.deseille@ig2i.centralelille.fr>
2023-02-12 13:07:03 +0100

    fonctions vu en cours ajoutées

--- biblio.c
+++ biblio.c
@@ -2,19 +2,31 @@
 
 void init (T_Bibliotheque *ptrB)
 {
-    
+  ptrB -> nbLivres = 0;  
 }
 
 
 int ajouterLivre(T_Bibliotheque  *ptrB)
 {
-    
+
+    if( ptrB -> nbLivres < CAPACITE_BIBLIO){
+        saisirLivre(&(ptrB -> etagere[ptrB -> nbLivres]));
+        ptrB -> nbLivres++;
+        return 1;
+    }
     return 0;
 }
 
 
 int  afficherBibliotheque(const T_Bibliotheque  *ptrB)
 {
-    
+    int i;
+
+    if(ptrB -> nbLivres > 0){
+        for( i = 0; i < ptrB -> nbLivres; i++)
+            afficherLivre(&(ptrB -> etagere[i]));
+        return 1;
+    }
     return 0;
 }
+
